a5c6cf2271f699fd93dff205a8416feede5a4600
JonatanDZ <99177934+JonatanDZ@users.noreply.github.com>
2023-09-13 10:56:40 +0200

    demo

---
+++ demo_time.py
@@ -0,0 +1,15 @@
+import drivers
+from time import sleep
+from datetime import  datetime
+
+display = drivers.Lcd()
+
+try:
+    print("Skriver til LCD skærmen")
+    display.lcd_display_string("Ingen tid at spilde", 1)
+    while True:
+        display.lcd_display_string(str(datetime.now().time()),2)
+    
+except KeyboardInterrupt:
+    print("Lukker uret")
+    display.lcd_clear()
