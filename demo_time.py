7b8577c6741576559c4b83e8bbfae93da128b18d
JonatanDZ <99177934+JonatanDZ@users.noreply.github.com>
2023-09-13 11:07:54 +0200

    import drivers

--- demo_time.py
+++ demo_time.py
@@ -5,7 +5,7 @@ from datetime import  datetime
 display = drivers.Lcd()
 
 try:
-    print("Skriver til LCD skærmen")
+    print("Skriver til LCD skaermen")
     display.lcd_display_string("Ingen tid at spilde", 1)
     while True:
         display.lcd_display_string(str(datetime.now().time()),2)
