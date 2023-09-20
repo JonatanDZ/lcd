3735642c627c4bbdba9bf3a2a0b6e73ee722747d
JonatanDZ <99177934+JonatanDZ@users.noreply.github.com>
2023-09-20 11:12:39 +0200

    fix

--- demo_time.py
+++ demo_time.py
@@ -12,6 +12,7 @@ try:
         display.lcd_display_string("   Klokken er:", 1)
         display.lcd_display_string(f'    {now.replace(microsecond=0)}',2)
         sleep(6)
+        now = datetime.now().time()
         display.lcd_display_string("Det regner i dag", 1)   # Refresh the first line of display with a different message
         sleep(6)                                           # Give time for the message to be read
         display.lcd_clear()                                # Clear the display of any data
