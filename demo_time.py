444df98e04c177303e191ec8216f32b3721ee3e2
JonatanDZ <99177934+JonatanDZ@users.noreply.github.com>
2023-09-20 10:54:50 +0200

    fix

--- demo_time.py
+++ demo_time.py
@@ -3,13 +3,14 @@ from time import sleep
 from datetime import  datetime
 
 display = drivers.Lcd()
+now = datetime.now().time()
 
 try:
     print("Skriver til LCD skaermen")
     display.lcd_display_string("Dato: ", 1)
     while True:
         display.lcd_display_string("   Klokken er:", 1)
-        display.lcd_display_string(str(datetime.now().isoformat(timespec="seconds")),2)
+        display.lcd_display_string(str(unicode(now.replace(microsecond=0))),2)
         sleep(6)
         display.lcd_display_string("Det regner i dag", 1)   # Refresh the first line of display with a different message
         sleep(6)                                           # Give time for the message to be read
