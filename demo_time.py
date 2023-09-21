cc39dd8d335d1f862a9472521f10a8c2d350f0a3
JonatanDZ <99177934+JonatanDZ@users.noreply.github.com>
2023-09-21 12:31:18 +0200

    fix multiprocess

--- demo_time.py
+++ demo_time.py
@@ -1,22 +1,31 @@
 import drivers
 from time import sleep
 from datetime import  datetime
+from multiprocessing import Process
 
 display = drivers.Lcd()
 
-try:
-    print("Skriver til LCD skaermen")
+def updateTime():
     while True:
         now = datetime.now().time()
-        display.lcd_display_string("   Klokken er:", 1)
         display.lcd_display_string(f'    {now.replace(microsecond=0)}',2)
+
+p = Process(target=updateTime)
+
+try:
+    print("Skriver til LCD skaermen")
+    
+    p.start()
+
+    while True:
+        display.lcd_display_string("   Klokken er:", 1)
         sleep(6)
-        now = datetime.now().time()
         display.lcd_display_string("Det regner i dag", 1)   # Refresh the first line of display with a different message
         sleep(6)                                           # Give time for the message to be read
         display.lcd_clear()                                # Clear the display of any data
         sleep(1)   
-    
+
 except KeyboardInterrupt:
     print("Slukker for uret")
     display.lcd_clear()
+
