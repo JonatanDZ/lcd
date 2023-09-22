38a7e9191eaaff535ad5c0859bc101692c43a354
JonatanDZ <99177934+JonatanDZ@users.noreply.github.com>
2023-09-22 12:17:28 +0200

    fix

--- demo_time.py
+++ demo_time.py
@@ -2,9 +2,27 @@ import drivers
 from time import sleep
 from datetime import  datetime
 from multiprocessing import Process
+import requests
 
 display = drivers.Lcd()
 p = Process(target=updateTime)
+api_key = '46191dbc779cc644b3ef1468a6a5e1a5'
+city = 'Struer'
+
+def fetch_weather():
+    api_url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+    try:
+        response = requests.get(api_url)
+        data = response.json()
+        weather = data['weather'][0]['description']
+        return weather
+    except Exception as e:
+        print('Error fetching weather data:', e)
+
+# To use the function and get the weather description:
+weather_description = fetch_weather()
+if weather_description:
+    print(f'Weather: {weather_description}')
 
 def updateTime():
     while True:
@@ -22,7 +40,7 @@ try:
     while True:
         display.lcd_display_string("   Klokken er:  ", 1)
         sleep(6)
-        display.lcd_display_string("Det regner i dag", 1)
+        display.lcd_display_string(f'Weather: {weather_description}', 1)
         sleep(6)
 
 except KeyboardInterrupt:
